33aa5f11687962154cda09d2cf244a3b37f9993f
maxym_ko <koval.maxim@unicyb.kiev.ua>
2021-11-19 04:11:25 +0200

    [OMO] [Lab-3] Dot product method implementatiob

--- 3rd_course/labs_omo/lab3/src/main/java/csc/nonlinear/system/Main.java
+++ 3rd_course/labs_omo/lab3/src/main/java/csc/nonlinear/system/Main.java
@@ -1,14 +1,19 @@
 package csc.nonlinear.system;
 
 import Jama.Matrix;
+import csc.linear.system.domain.Vector;
+import csc.linear.system.util.MatrixUtils;
+import csc.linear.system.util.VectorUtils;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        demoNewton();
+        demoNewtonMethod();
+        System.out.println("\n------------------------------------------------------\n");
+        demoDotProductMethod();
     }
 
-    private static void demoNewton() throws Exception {
+    private static void demoNewtonMethod() throws Exception {
         String f1 = "5 * x - 6 * y + 20 * LN(x) + 16";
         String f2 = "2 * x + y -10 * LN(y) - 4";
         Matrix result = NewtonMethod.solve(f1, f2, 1, 1, 0.001);
@@ -16,6 +21,16 @@ public class Main {
         double x = result.get(0, 0);
         double y = result.get(1, 0);
 
+        System.out.println("The system of equations:\n" + f1 + " = 0\n" + f2 + " = 0\n");
         System.out.println("Answer vector: (" + x + "; " + y + ")");
+    }
+
+    private static void demoDotProductMethod() {
+        csc.linear.system.domain.Matrix a = MatrixUtils.generateDiagonallyDominantMatrix(4, 50, true);
+        Vector x0 = VectorUtils.getIdentityVector(4);
+
+        System.out.println("Matrix:\n" + a + "\n");
+        System.out.println("Max eigenvalue: " + DotProductMethod.findMaxEigenValue(a, x0, 0.001));
+        System.out.println("Min eigenvalue:" + DotProductMethod.findMinEigenValue(a, x0, 0.001));
     }
 }
